69d654ebd2eef089f8bd6018a8b705d6a45521e7
BART! <bart@bartweb.cz>
2019-11-09 01:56:00 +0200

    chore(actions): use gpr

--- scripts/gpr.js
+++ scripts/gpr.js
@@ -1,13 +1,12 @@
 const fs = require('fs')
 const { join } = require('path')
 
-const pkg = require('../dist/ngx-scrolltop/package.json')
+const pkgDistPath = '../dist/ngx-scrolltop/package.json'
+
+const pkg = require(pkgDistPath)
 
 // pkg.name = `@bartholomej/${pkg.name}`
 pkg.publishConfig = {}
 pkg.publishConfig.registry = `https://npm.pkg.github.com/@bartholomej`
 
-fs.writeFileSync(
-  join(__dirname, '../package.json'),
-  JSON.stringify(pkg, null, 2)
-)
+fs.writeFileSync(join(__dirname, pkgDistPath), JSON.stringify(pkg, null, 2))
